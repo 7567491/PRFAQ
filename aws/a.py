72a31a79a3b759ebb24cc8520638f90ef834b658
7567491 <lu@amazonsp.com>
2024-11-22 10:40:28 +0800

    测通了，真不容易

--- aws/a.py
+++ aws/a.py
@@ -7,7 +7,7 @@ data = {
 }
 
 # 修改请求路径为 /mp
-response = requests.post('http://localhost:8000/mp', json=data, allow_redirects=False)
+response = requests.post('http://amazonsp.com/mp', json=data, allow_redirects=False)
 print(f"发送请求完成，响应状态码: {response.status_code}")
 
 # 检查是否是重定向响应
